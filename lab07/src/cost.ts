36f3305c7e8a4230c4a7e8ed7383a4dba9bf44eb
mashawarface <sony.sky.16@gmail.com>
2025-12-07 14:57:31 +0700

    added cost func

--- lab07/src/cost.ts
+++ lab07/src/cost.ts
@@ -1,6 +1,16 @@
 import { Expr } from "../../lab04";
 
-export function cost(e: Expr): number
-{
-    throw "Not implemented";
+export function cost(e: Expr): number {
+  switch (e.type) {
+    case "num":
+      return 0;
+    case "var":
+      return 1;
+    case "binary":
+      return 1 + cost(e.left) + cost(e.right);
+    case "unary":
+      return 1 + cost(e.arg);
+    default:
+      throw new Error(`Uknown expression type: ${(e as any).type}!`);
+  }
 }
